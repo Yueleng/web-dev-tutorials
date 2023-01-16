4b32616a19dd1ce0e90379115866d50ee840317e
wangyueleng@gmail.com <wangyueleng@gmail.com>
2023-01-15 21:23:58 -0500

    ts type challenge/objectentries: update infer usage and another solution

--- tdl-ts/type-challenges/02946-medium-objectentries/template.ts
+++ tdl-ts/type-challenges/02946-medium-objectentries/template.ts
@@ -17,17 +17,28 @@ type ModelEntries =
 type objectEntries = ObjectEntries1<Partial<Model>>; // ["name", string | undefined] | ["age", number | undefined] | ["locations", string[] | null | undefined]
 type objectEntries2 = ObjectEntries1<{ key?: undefined }>;
 
-// 牛逼啊
+// 牛逼啊, infer 可以这么用？
 type ObjectEntries2<T, K extends keyof T = keyof T> = K extends K
   ? [K, T[K] extends undefined | infer Type ? Type : undefined]
   : never;
 
 // 666
-type ObjectEntries<T extends object> = {
+type ObjectEntries3<T extends object> = {
   [key in keyof Required<T>]: [
     key,
     Required<T>[key] extends never ? T[key] : Required<T>[key]
   ];
 }[keyof T];
 
-type objectEntries3 = ObjectEntries<{ key?: undefined }>;
+type objectEntries3 = ObjectEntries3<{ key?: undefined }>;
+
+type ObjectEntries<T> = {
+  [Key in keyof T]-?: [
+    Key,
+    T[Key] extends undefined | infer Type ? Type : undefined
+  ];
+}[keyof T];
+
+// infer 的用法
+type ___a = undefined extends undefined | "a" ? true : false; // true
+type ___b = undefined extends undefined | infer Type ? Type : undefined; // undefined
