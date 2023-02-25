9ba7ba4b40f3cddfe056eadf9b606f3807816d85
Alan <wangyueleng@gmail.com>
2023-02-25 15:41:30 -0500

    full-stack-react-graphql-ts-node/graph-ql
    - add package: express/node server
    - add package: apollo server
    - add package: cors & body parser (required by express)
    - add package: graphql/type-graphql
    - feat: start express server, attach graphql apollo server, create the simplest graphql resolver (hello word)

--- full-stack-react-graphql-ts/reddit-server/src/index.ts
+++ full-stack-react-graphql-ts/reddit-server/src/index.ts
@@ -1,17 +1,43 @@
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "@apollo/server";
+import { buildSchema } from "type-graphql";
+import { expressMiddleware } from "@apollo/server/express4";
+import cors from "cors";
+import { json } from "body-parser";
+import { HelloResolver } from "./resolvers/hello";
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
 import mikroOrmConfig from "./mikro-orm.config";
 
 const main = async () => {
   const orm = await MikroORM.init(mikroOrmConfig);
   await orm.getMigrator().up();
-  const emFork = orm.em.fork(); // <-- create the fork
 
-  //   const post = emFork.create(Post, { title: "My first post" });
-  //   await emFork.persistAndFlush(post);
-  const posts = await emFork.find(Post, {});
-  console.log(posts);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver],
+      validate: false,
+    }),
+  });
+
+  await apolloServer.start();
+
+  app.use(
+    "/graphql",
+    cors<cors.CorsRequest>(),
+    json(),
+    expressMiddleware(apolloServer)
+  );
+
+  app.get("/", (_, res) => {
+    res.send("hello");
+  });
+
+  app.listen(4000, () => {
+    console.log("Server started! Listening on localhost:4000");
+  });
 };
 
 main();
