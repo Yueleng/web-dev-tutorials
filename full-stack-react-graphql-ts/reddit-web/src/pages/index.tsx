c99ab1ba99950f8878d7b580c88b577a6bdb5e50
Alan <wangyueleng@gmail.com>
2023-04-16 15:55:27 -0400

    full-stack-react-graphql-ts-node/frontend
    - init register page
    - install formik

--- full-stack-react-graphql-ts/reddit-web/src/pages/index.tsx
+++ full-stack-react-graphql-ts/reddit-web/src/pages/index.tsx
@@ -16,41 +16,42 @@ import { CTA } from '../components/CTA'
 import { Footer } from '../components/Footer'
 
 const Index = () => (
-  <Container height="100vh">
-    <Hero />
-    <Main>
-      <Text color="text">
-        Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
-        <Code>TypeScript</Code>.
-      </Text>
+  // <Container height="100vh">
+  //   <Hero />
+  //   <Main>
+  //     <Text color="text">
+  //       Example repository of <Code>Next.js</Code> + <Code>chakra-ui</Code> +{' '}
+  //       <Code>TypeScript</Code>.
+  //     </Text>
 
-      <List spacing={3} my={0} color="text">
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink
-            isExternal
-            href="https://chakra-ui.com"
-            flexGrow={1}
-            mr={2}
-          >
-            Chakra UI <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-        <ListItem>
-          <ListIcon as={CheckCircleIcon} color="green.500" />
-          <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
-            Next.js <LinkIcon />
-          </ChakraLink>
-        </ListItem>
-      </List>
-    </Main>
+  //     <List spacing={3} my={0} color="text">
+  //       <ListItem>
+  //         <ListIcon as={CheckCircleIcon} color="green.500" />
+  //         <ChakraLink
+  //           isExternal
+  //           href="https://chakra-ui.com"
+  //           flexGrow={1}
+  //           mr={2}
+  //         >
+  //           Chakra UI <LinkIcon />
+  //         </ChakraLink>
+  //       </ListItem>
+  //       <ListItem>
+  //         <ListIcon as={CheckCircleIcon} color="green.500" />
+  //         <ChakraLink isExternal href="https://nextjs.org" flexGrow={1} mr={2}>
+  //           Next.js <LinkIcon />
+  //         </ChakraLink>
+  //       </ListItem>
+  //     </List>
+  //   </Main>
 
-    <DarkModeSwitch />
-    <Footer>
-      <Text>Next ❤️ Chakra</Text>
-    </Footer>
-    <CTA />
-  </Container>
+  //   <DarkModeSwitch />
+  //   <Footer>
+  //     <Text>Next ❤️ Chakra</Text>
+  //   </Footer>
+  //   <CTA />
+  // </Container>
+  <div>Hello World</div>
 )
 
 export default Index
---
+++ full-stack-react-graphql-ts/reddit-web/src/pages/register.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+interface RegisterProps {
+  props1: string;
+  props2: string;
+}
+
+const Register: React.FC<RegisterProps> = ({ props1, props2 }) => {
+  return <div>Register User</div>;
+};
+
+export default Register;
