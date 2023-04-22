924a1ab7b734af52bb0d8284016b777b72c6a882
Alan <wangyueleng@gmail.com>
2023-04-22 15:27:24 -0400

    learn-spring-boot
    - JSP

--- learn-spring/ToDo/src/main/java/com/alansoftware/todo/hello/SayHelloController.java
+++ learn-spring/ToDo/src/main/java/com/alansoftware/todo/hello/SayHelloController.java
@@ -29,4 +29,12 @@ public class SayHelloController {
         return sb.toString();
     }
 
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/sayHello.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/welcome.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/login.jsp
+    // /src/main/resources/META-INF/resources/WEB-INF/jsp/todos.jsp
+    @RequestMapping("say-hello-jsp")
+    public String sayHelloJSP() {
+        return "sayHello";
+    }
 }
---
+++ learn-spring/ToDo/src/main/java/com/alansoftware/todo/login/LoginController.java
@@ -0,0 +1,12 @@
+package com.alansoftware.todo.login;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+    @RequestMapping("login-page")
+    public String login() {
+        return "login";
+    }
+}
