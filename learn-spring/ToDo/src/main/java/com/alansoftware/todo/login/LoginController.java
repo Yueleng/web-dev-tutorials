d22a498f393e073c82a67a543e6fd1c20e98ad4b
Alan <wangyueleng@gmail.com>
2023-04-22 17:20:56 -0400

    learn-spring-boot
    - slf4j logger

--- learn-spring/ToDo/src/main/java/com/alansoftware/todo/login/LoginController.java
+++ learn-spring/ToDo/src/main/java/com/alansoftware/todo/login/LoginController.java
@@ -1,5 +1,7 @@
 package com.alansoftware.todo.login;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,12 +10,14 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class LoginController {
 
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
     // login-page => com.alansoftware.todo.login.loginController => request mapping (login-page)
     // => login.jsp
     @RequestMapping("login-page")
     public String login(@RequestParam String name, ModelMap model) {
         model.put("name", name);
-        System.out.println("Reqest param is " + name); // Not recommended for PROD CODE
+        logger.debug("Request param is {}", name);
         return "login";
     }
 }
