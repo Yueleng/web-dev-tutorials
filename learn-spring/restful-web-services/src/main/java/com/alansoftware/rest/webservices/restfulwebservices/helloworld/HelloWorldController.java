f08e40d828d25c030e1f01908c895141e6e082b0
Alan <wangyueleng@gmail.com>
2023-05-06 15:44:36 -0400

    learn-spring-boot
    - introducing @Pathvariable to math the path value

--- learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -1,9 +1,6 @@
 package com.alansoftware.rest.webservices.restfulwebservices.helloworld;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 // REST API
 @RestController
@@ -25,6 +22,15 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
+    }
+
+    // Path Parameters
+    // /users/{id}/todos/{id}  => /users/2/todos/200
+    // /hello-world/path-variable/{name}
+
+    @GetMapping(path = "/hello-world/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 
 }
