a28a6e279076f93fd4dbea7e94f9044e0150360e
Alan <wangyueleng@gmail.com>
2023-05-06 15:18:10 -0400

    learn-spring-boot
    - return a bean instead of string as a result (spring boot transformed into json)

--- learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -22,5 +22,9 @@ public class HelloWorldController {
         return "Hello World with GetMapping";
     }
 
+    @GetMapping(path = "/hello-world-bean")
+    public HelloWorldBean helloWorldBean() {
+        return new HelloWorldBean("Hello World");
+    }
 
 }
