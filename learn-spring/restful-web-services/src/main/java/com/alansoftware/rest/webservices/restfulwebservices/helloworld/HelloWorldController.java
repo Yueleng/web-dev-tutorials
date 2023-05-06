de9319f849256c27586a7b278a3ae5f67d318b85
Alan <wangyueleng@gmail.com>
2023-05-06 15:10:13 -0400

    learn-spring-boot
    - Create first api: hello world api

---
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -0,0 +1,26 @@
+package com.alansoftware.rest.webservices.restfulwebservices.helloworld;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+// REST API
+@RestController
+public class HelloWorldController {
+
+    // hello world
+
+    // "Hello World"
+    @RequestMapping(method = RequestMethod.GET, path="/hello-world")
+    public String helloWorld() {
+        return "Hello World";
+    }
+
+    @GetMapping(path = "/hello-world2")
+    public String helloWorld2() {
+        return "Hello World with GetMapping";
+    }
+
+
+}
