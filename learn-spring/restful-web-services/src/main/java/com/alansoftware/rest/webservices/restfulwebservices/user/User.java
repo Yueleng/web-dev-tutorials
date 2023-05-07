21e31043807a68fcba527485342d9ce196cf31c3
Alan <wangyueleng@gmail.com>
2023-05-07 16:04:53 -0400

    learn-spring-boot
    - introduce H2 In-memory DB

--- learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/user/User.java
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/user/User.java
@@ -1,12 +1,19 @@
 package com.alansoftware.rest.webservices.restfulwebservices.user;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
+@Entity(name="user_details")
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
     @Size(min=2, message = "Name should have at least 2 characters")
     @JsonProperty("user_name")
@@ -15,6 +22,10 @@ public class User {
     @Past(message = "Birth Date should be in the past")
     @JsonProperty("birth_date")
     private LocalDate birthDate;
+
+    protected User() {
+
+    }
 
     public User(Integer id, String name, LocalDate birthDate) {
         this.id = id;
