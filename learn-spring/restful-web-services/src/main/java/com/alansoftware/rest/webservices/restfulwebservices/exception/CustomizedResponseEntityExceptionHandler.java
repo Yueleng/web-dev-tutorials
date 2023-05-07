9c52b6303e014d78e8b6fedeea216f07f5ac9bca
Alan <wangyueleng@gmail.com>
2023-05-06 21:40:41 -0400

    learn-spring-boot
    - validation and new method: handleMethodArgumentNotValid

--- learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -1,8 +1,12 @@
 package com.alansoftware.rest.webservices.restfulwebservices.exception;
 
 import com.alansoftware.rest.webservices.restfulwebservices.user.UserNotFoundException;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
@@ -26,5 +30,17 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
         ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), ex.getMessage(), request.getDescription(false));
 
         return new ResponseEntity<>(errorDetails, HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException ex,
+            HttpHeaders headers,
+            HttpStatusCode status,
+            WebRequest request
+    ) {
+        String errorMsg = "Total Errors: " + ex.getErrorCount() + ". First Error: " + ex.getFieldError().getDefaultMessage();
+        ErrorDetails errorDetails = new ErrorDetails(LocalDateTime.now(), errorMsg, request.getDescription(false));
+        return new ResponseEntity<>(errorDetails, HttpStatus.BAD_REQUEST);
     }
 }
--- learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/user/UserResource.java
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.alansoftware.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -33,7 +34,7 @@ public class UserResource {
 
     // POST /users
     @PostMapping(path = "/users")
-    public ResponseEntity<Object> createUser(@RequestBody User user) {
+    public ResponseEntity<Object> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         // return location:
         // /users/4 => /users , user.getID
