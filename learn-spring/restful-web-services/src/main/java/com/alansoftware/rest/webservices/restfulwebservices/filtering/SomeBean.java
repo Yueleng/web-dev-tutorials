5a470d279b7ece622c5e8350d681941745fd1cf1
Alan <wangyueleng@gmail.com>
2023-05-07 13:33:11 -0400

    learn-spring-boot
    - implement fields filtering

---
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,34 @@
+package com.alansoftware.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+// @JsonIgnoreProperties({"field1", "field2})
+@JsonIgnoreProperties("field1")
+public class SomeBean {
+
+    private String field1;
+    @JsonIgnore
+    private String field2;
+
+    private String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+}
--- learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/user/User.java
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/user/User.java
@@ -1,5 +1,6 @@
 package com.alansoftware.rest.webservices.restfulwebservices.user;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
@@ -8,9 +9,11 @@ import java.time.LocalDate;
 public class User {
     private Integer id;
     @Size(min=2, message = "Name should have at least 2 characters")
+    @JsonProperty("user_name")
     private String name;
 
     @Past(message = "Birth Date should be in the past")
+    @JsonProperty("birth_date")
     private LocalDate birthDate;
 
     public User(Integer id, String name, LocalDate birthDate) {
