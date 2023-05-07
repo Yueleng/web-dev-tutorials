a85c4493d7955e9e3b4842ca734055a8a820c608
Alan <wangyueleng@gmail.com>
2023-05-07 12:47:40 -0400

    learn-spring-boot
    - api versioning using header: X-API-VERSION & ACCEPT

--- learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
+++ learn-spring/restful-web-services/src/main/java/com/alansoftware/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -16,4 +16,33 @@ public class VersioningPersonController {
         return new PersonV2(new Name("Janine", "Wu"));
     }
 
+    @GetMapping(path = "/person", params = "version=1")
+    public PersonV1 getFirstVersionOfPersonRequestParameter() {
+        return new PersonV1("Janine Wu");
+    }
+
+    @GetMapping(path = "/person", params = "version=2")
+    public PersonV2 getSecondVersionOfPersonRequestParameter() {
+        return new PersonV2(new Name("Janine", "Wu"));
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=1")
+    public PersonV1 getFirstVersionOfPersonRequestHeader() {
+        return new PersonV1("Janine Wu");
+    }
+
+    @GetMapping(path = "/person/header", headers = "X-API-VERSION=2")
+    public PersonV2 getSecondVersionOfPersonRequestHeader() {
+        return new PersonV2(new Name("Janine", "Wu"));
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v1+json")
+    public PersonV1 getFirstVersionOfPersonAcceptHeader() {
+        return new PersonV1("Janine Wu");
+    }
+
+    @GetMapping(path = "/person/accept", produces = "application/vnd.company.app-v2+json")
+    public PersonV2 getSecondVersionOfPersonAcceptHeader() {
+        return new PersonV2(new Name("Janine", "Wu"));
+    }
 }
